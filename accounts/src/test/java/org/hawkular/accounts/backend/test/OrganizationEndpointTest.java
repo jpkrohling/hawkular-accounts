b457e484bb95861acf522e3deae08db0d63c9124
Juraci Paixão Kröhling <juraci@kroehling.de>
2016-04-27 17:19:37 +0200

    ported more tests

--- accounts/src/test/java/org/hawkular/accounts/backend/test/OrganizationEndpointTest.java
+++ accounts/src/test/java/org/hawkular/accounts/backend/test/OrganizationEndpointTest.java
@@ -18,38 +18,60 @@ package org.hawkular.accounts.backend.test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.UUID;
 
 import javax.inject.Inject;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.ws.rs.core.Response;
 
+import org.hawkular.accounts.api.PersonaService;
+import org.hawkular.accounts.api.UserService;
 import org.hawkular.accounts.api.model.HawkularUser;
 import org.hawkular.accounts.api.model.Organization;
+import org.hawkular.accounts.api.model.OrganizationJoinRequest;
 import org.hawkular.accounts.api.model.Visibility;
 import org.hawkular.accounts.backend.boundary.OrganizationEndpoint;
+import org.hawkular.accounts.backend.boundary.OrganizationJoinEndpoint;
+import org.hawkular.accounts.backend.boundary.OrganizationMembershipEndpoint;
+import org.hawkular.accounts.backend.entity.rest.OrganizationJoinRequestDecisionRequest;
 import org.hawkular.accounts.backend.entity.rest.OrganizationRequest;
 import org.jboss.arquillian.junit.Arquillian;
-import org.jboss.arquillian.test.api.ArquillianResource;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import com.icegreen.greenmail.junit.GreenMailRule;
+import com.icegreen.greenmail.util.ServerSetupTest;
 
 /**
  * @author Juraci Paixão Kröhling
  */
 @RunWith(Arquillian.class)
 public class OrganizationEndpointTest extends BaseEndpointTest {
-
-    @Inject
-    OrganizationEndpoint endpoint;
-
-    @ArquillianResource
-    URL baseUrl;
+    @Rule
+    public final GreenMailRule greenMail = new GreenMailRule(ServerSetupTest.ALL);
 
     @Inject
     ProducersForTest producersForTest;
+
+    @Inject
+    OrganizationEndpoint organizationEndpoint;
+
+    @Inject
+    OrganizationMembershipEndpoint organizationMembershipEndpoint;
+
+    @Inject
+    OrganizationJoinEndpoint organizationJoinEndpoint;
+
+    @Inject
+    UserService userService;
+
+    @Inject
+    PersonaService personaService;
 
     @Test
     public void createOrganizationWithoutVisibility() {
@@ -57,7 +79,7 @@ public class OrganizationEndpointTest extends BaseEndpointTest {
 
         OrganizationRequest request = new OrganizationRequest();
         request.setName(orgName);
-        Response response = endpoint.createOrganization(request);
+        Response response = organizationEndpoint.createOrganization(request);
         assertEquals(200, response.getStatus());
         Organization created = (Organization) response.getEntity();
         assertNotNull(created.getId());
@@ -73,7 +95,7 @@ public class OrganizationEndpointTest extends BaseEndpointTest {
         request.setName(orgName);
         request.setVisibility(Visibility.APPLY.name());
 
-        Response response = endpoint.createOrganization(request);
+        Response response = organizationEndpoint.createOrganization(request);
         assertEquals(200, response.getStatus());
         Organization created = (Organization) response.getEntity();
         assertNotNull(created.getId());
@@ -89,7 +111,7 @@ public class OrganizationEndpointTest extends BaseEndpointTest {
         request.setName(orgName);
         request.setVisibility(Visibility.PRIVATE.name());
 
-        Response response = endpoint.createOrganization(request);
+        Response response = organizationEndpoint.createOrganization(request);
         assertEquals(200, response.getStatus());
         Organization created = (Organization) response.getEntity();
         assertNotNull(created.getId());
@@ -98,31 +120,201 @@ public class OrganizationEndpointTest extends BaseEndpointTest {
     }
 
     @Test
-    public void jsmithAppliesForOrganization() throws URISyntaxException {
-        HawkularUser jdoe = new HawkularUser(UUID.randomUUID().toString());
-        HawkularUser jsmith = new HawkularUser(UUID.randomUUID().toString());
+    public void jsmithAppliesForOrganization() throws URISyntaxException, MessagingException {
+        HawkularUser jdoe = new HawkularUser(UUID.randomUUID(), "jdoe");
+        HawkularUser jsmith = new HawkularUser(UUID.randomUUID(), "jsmith");
+
+        jdoe.setEmail("jdoe@example.com");
+        jsmith.setEmail("jsmith@example.com");
+        userService.update(jdoe);
+        userService.update(jsmith);
 
         producersForTest.setHawkularUser(jdoe);
         producersForTest.setPersona(jdoe);
         String orgName = UUID.randomUUID().toString();
         OrganizationRequest request = new OrganizationRequest();
         request.setName(orgName);
-        request.setVisibility(Visibility.PRIVATE.name());
-        Response response = endpoint.createOrganization(request);
+        request.setVisibility(Visibility.APPLY.name());
+        Response response = organizationEndpoint.createOrganization(request);
         assertEquals(200, response.getStatus());
-        Organization created = (Organization) response.getEntity();
-        assertEquals(jdoe.getId(), created.getOwner().getId());
+        Organization organization = (Organization) response.getEntity();
+        assertEquals(jdoe.getId(), organization.getOwner().getId());
 
         producersForTest.setHawkularUser(jsmith);
         producersForTest.setPersona(jsmith);
-        orgName = UUID.randomUUID().toString();
-        request = new OrganizationRequest();
+        Response joinApplicationResponse = organizationJoinEndpoint.applyToJoin(organization.getId());
+        OrganizationJoinRequest joinRequest = (OrganizationJoinRequest) joinApplicationResponse.getEntity();
+        assertEquals(200, joinApplicationResponse.getStatus());
+        assertNotNull(joinRequest.getId());
+        assertEquals("PENDING", joinRequest.getStatus().name());
+        assertTrue(greenMail.waitForIncomingEmail(5000, 2));
+
+        // we expect two messages: one to the user who sent the join request, and one to the admin
+        // both have the same subject... if the subject ever changes, we need to change the test as well
+        for (MimeMessage message : greenMail.getReceivedMessages()) {
+            assertTrue("Expected to send a join request notification, but got another message",
+                    message.getSubject().startsWith("[hawkular] - Join request for"));
+        }
+
+        producersForTest.setHawkularUser(jdoe);
+        producersForTest.setPersona(jdoe);
+
+        OrganizationJoinRequestDecisionRequest joinRequestDecisionRequest = new
+                OrganizationJoinRequestDecisionRequest();
+        joinRequestDecisionRequest.setDecision("ACCEPT");
+        joinRequestDecisionRequest.setJoinRequestId(joinRequest.getId());
+        Response requestDecisionResponse =  organizationJoinEndpoint.requestDecision(
+                joinRequestDecisionRequest,
+                organization.getId()
+        );
+        OrganizationJoinRequest joinRequestAfterDecision = (OrganizationJoinRequest) requestDecisionResponse
+                .getEntity();
+        assertEquals(200, requestDecisionResponse.getStatus());
+        assertNotNull(joinRequestAfterDecision.getId());
+        assertEquals("ACCEPTED", joinRequestAfterDecision.getStatus().name());
+        assertTrue(greenMail.waitForIncomingEmail(5000, 2));
+        MimeMessage message = greenMail.getReceivedMessages()[2];
+        String expectedSubject = "[hawkular] - You have been accepted to join";
+        assertTrue("Should have received an approval email", message.getSubject().startsWith(expectedSubject));
+
+        assertTrue(personaService.isAllowedToImpersonate(jsmith, organization));
+    }
+
+    @Test
+    public void jsmithGetsApprovedTwice() {
+        HawkularUser jdoe = new HawkularUser(UUID.randomUUID(), "jdoe");
+        HawkularUser jsmith = new HawkularUser(UUID.randomUUID(), "jsmith");
+
+        jdoe.setEmail("jdoe.jsmithGetsApprovedTwice@example.com");
+        jsmith.setEmail("jsmith.jsmithGetsApprovedTwice@example.com");
+        userService.update(jdoe);
+        userService.update(jsmith);
+
+        producersForTest.setHawkularUser(jdoe);
+        producersForTest.setPersona(jdoe);
+        String orgName = UUID.randomUUID().toString();
+        OrganizationRequest request = new OrganizationRequest();
         request.setName(orgName);
-        request.setVisibility(Visibility.PRIVATE.name());
-        response = endpoint.createOrganization(request);
-        assertEquals(200, response.getStatus());
-        created = (Organization) response.getEntity();
-        assertEquals(jsmith.getId(), created.getOwner().getId());
+        request.setVisibility(Visibility.APPLY.name());
+        Response response = organizationEndpoint.createOrganization(request);
+        Organization organization = (Organization) response.getEntity();
+
+        producersForTest.setHawkularUser(jsmith);
+        producersForTest.setPersona(jsmith);
+        Response joinApplicationResponse = organizationJoinEndpoint.applyToJoin(organization.getId());
+        OrganizationJoinRequest joinRequestAfterDecision = (OrganizationJoinRequest) joinApplicationResponse.getEntity();
+
+        producersForTest.setHawkularUser(jdoe);
+        producersForTest.setPersona(jdoe);
+        OrganizationJoinRequestDecisionRequest joinRequestDecisionRequest = new
+                OrganizationJoinRequestDecisionRequest();
+        joinRequestDecisionRequest.setDecision("ACCEPT");
+        joinRequestDecisionRequest.setJoinRequestId(joinRequestAfterDecision.getId());
+
+        Response requestDecisionResponse =  organizationJoinEndpoint.requestDecision(
+                joinRequestDecisionRequest,
+                organization.getId()
+        );
+        assertEquals(200, requestDecisionResponse.getStatus());
+
+        requestDecisionResponse =  organizationJoinEndpoint.requestDecision(
+                joinRequestDecisionRequest,
+                organization.getId()
+        );
+        assertEquals(202, requestDecisionResponse.getStatus());
+
+    }
+
+    @Test
+    public void jsmithGetsRejectedAndThenAccepted() {
+        HawkularUser jdoe = new HawkularUser(UUID.randomUUID(), "jdoe");
+        HawkularUser jsmith = new HawkularUser(UUID.randomUUID(), "jsmith");
+
+        jdoe.setEmail("jdoe.jsmithGetsApprovedTwice@example.com");
+        jsmith.setEmail("jsmith.jsmithGetsApprovedTwice@example.com");
+        userService.update(jdoe);
+        userService.update(jsmith);
+
+        producersForTest.setHawkularUser(jdoe);
+        producersForTest.setPersona(jdoe);
+        String orgName = UUID.randomUUID().toString();
+        OrganizationRequest request = new OrganizationRequest();
+        request.setName(orgName);
+        request.setVisibility(Visibility.APPLY.name());
+        Response response = organizationEndpoint.createOrganization(request);
+        Organization organization = (Organization) response.getEntity();
+
+        producersForTest.setHawkularUser(jsmith);
+        producersForTest.setPersona(jsmith);
+        Response joinApplicationResponse = organizationJoinEndpoint.applyToJoin(organization.getId());
+        OrganizationJoinRequest joinRequestAfterDecision = (OrganizationJoinRequest) joinApplicationResponse.getEntity();
+
+        producersForTest.setHawkularUser(jdoe);
+        producersForTest.setPersona(jdoe);
+        OrganizationJoinRequestDecisionRequest joinRequestDecisionRequest = new
+                OrganizationJoinRequestDecisionRequest();
+        joinRequestDecisionRequest.setDecision("REJECT");
+        joinRequestDecisionRequest.setJoinRequestId(joinRequestAfterDecision.getId());
+
+        Response requestDecisionResponse =  organizationJoinEndpoint.requestDecision(
+                joinRequestDecisionRequest,
+                organization.getId()
+        );
+        assertEquals(200, requestDecisionResponse.getStatus());
+
+        joinRequestDecisionRequest.setDecision("ACCEPT");
+        requestDecisionResponse =  organizationJoinEndpoint.requestDecision(
+                joinRequestDecisionRequest,
+                organization.getId()
+        );
+        assertEquals(409, requestDecisionResponse.getStatus());
+
+    }
+
+    @Test
+    public void jsmithGetsAcceptedAndThenRejected() {
+        HawkularUser jdoe = new HawkularUser(UUID.randomUUID(), "jdoe");
+        HawkularUser jsmith = new HawkularUser(UUID.randomUUID(), "jsmith");
+
+        jdoe.setEmail("jdoe.jsmithGetsApprovedTwice@example.com");
+        jsmith.setEmail("jsmith.jsmithGetsApprovedTwice@example.com");
+        userService.update(jdoe);
+        userService.update(jsmith);
+
+        producersForTest.setHawkularUser(jdoe);
+        producersForTest.setPersona(jdoe);
+        String orgName = UUID.randomUUID().toString();
+        OrganizationRequest request = new OrganizationRequest();
+        request.setName(orgName);
+        request.setVisibility(Visibility.APPLY.name());
+        Response response = organizationEndpoint.createOrganization(request);
+        Organization organization = (Organization) response.getEntity();
+
+        producersForTest.setHawkularUser(jsmith);
+        producersForTest.setPersona(jsmith);
+        Response joinApplicationResponse = organizationJoinEndpoint.applyToJoin(organization.getId());
+        OrganizationJoinRequest joinRequestAfterDecision = (OrganizationJoinRequest) joinApplicationResponse.getEntity();
+
+        producersForTest.setHawkularUser(jdoe);
+        producersForTest.setPersona(jdoe);
+        OrganizationJoinRequestDecisionRequest joinRequestDecisionRequest = new
+                OrganizationJoinRequestDecisionRequest();
+        joinRequestDecisionRequest.setDecision("ACCEPT");
+        joinRequestDecisionRequest.setJoinRequestId(joinRequestAfterDecision.getId());
+
+        Response requestDecisionResponse =  organizationJoinEndpoint.requestDecision(
+                joinRequestDecisionRequest,
+                organization.getId()
+        );
+        assertEquals(200, requestDecisionResponse.getStatus());
+
+        joinRequestDecisionRequest.setDecision("REJECT");
+        requestDecisionResponse =  organizationJoinEndpoint.requestDecision(
+                joinRequestDecisionRequest,
+                organization.getId()
+        );
+        assertEquals(409, requestDecisionResponse.getStatus());
+
     }
 
 }
